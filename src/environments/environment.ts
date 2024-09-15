69175333763a2953e85efbfc2de4d83bcdcc0693
Evandro <svecontabilidade@gmail.com>
2024-09-15 10:48:08 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 
     urlApi: 'https://lojavirtualmentoria.com.br/loja_virtual_mentoria',
     urlApiLocal: 'http://localhost:8080/loja_virtual_mentoria',
-    urlApiLocalSSL: 'https://localhost:8443/loja_virtual_mentoria'
+    urlApiLocalSS: 'https://localhost:8443/loja_virtual_mentoria'
 }
